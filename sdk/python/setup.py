262a7b64cc3742c481e77fde6a263925b6938478
Ryan <ruifafan@gmail.com>
2026-02-17 21:53:45 +0800

    fix(python): 完善 setup.py 元数据，添加 long_description

--- sdk/python/setup.py
+++ sdk/python/setup.py
@@ -5,13 +5,18 @@ from setuptools import setup, find_packages
 requirements = [
     "mysql-connector-python==8.0.30",
     "psycopg2-binary==2.9.9"
-]
+
+# 读取 README.md 作为长描述
+with open(os.path.join(os.path.dirname(__file__), "README.md"), "r", encoding="utf-8") as fh:
+    long_description = fh.read()
 
 setup(
     name="easy-recon-sdk",
     version=os.getenv("SDK_VERSION", "1.0.0"),
     packages=find_packages(),
     description="Easy Recon SDK for Python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author="Coffers Tech",
     author_email="contact@cofferstech.com",
     url="https://github.com/coffersTech/easy-recon",
